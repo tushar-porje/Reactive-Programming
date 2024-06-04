92523b6ea29eda2b83b01cc645e3aa9e4833b32f
Tushar Porje <tusharporje13091999@gmail.com>
2024-06-04 23:42:19 +0530

    setup

---
+++ src/test/java/com/reactiveprogramming/webflux/webfluxproject/WebfluxprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactiveprogramming.webflux.webfluxproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
